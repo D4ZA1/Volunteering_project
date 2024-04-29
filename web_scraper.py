6a4a25a4bf8a2e861f992993b34a1421ae025611
Pasupuleti Jaswanth <139963394+D4ZA1@users.noreply.github.com>
2024-04-29 14:59:41 +0530

    done

--- web_scraper.py
+++ web_scraper.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 urls = []
 Details = []
+count = 0
 for i in range(10):
     urls.append(f'https://indiankanoon.org/search/?formInput=income%20tax%20judgements&pagenum={i}')
 for url in urls:
@@ -13,14 +14,13 @@ for url in urls:
     for link in soup.find_all('a'):
         if '/doc/' in link.get('href'):
             docs.append(link)
-
     for i in range(len(docs)):
         det = {}
         Url = 'https://indiankanoon.org' + docs[i].get('href')
         response = requests.get(Url)
         soup = BeautifulSoup(response.content, 'html.parser')
         for link in soup.find_all('div', class_='judgments'):
-            det['Judgment'] = f'{i+1}_Income_tax.pdf'
+            det['Judgment'] = f'{count+1}_Income_tax.pdf'
             det['Domain'] = 'Family Court'
             if link.find('h3', class_="doc_citations"):
                 det['Equivalent Citation'] = link.find('h3', class_="doc_citations").get_text()
@@ -37,8 +37,14 @@ for url in urls:
                 det['Bench'] = ''
             det['Court'] = link.find('h2', class_='docsource_main').get_text()
             det['link'] = Url
-
+        data = {
+            'type': 'pdf'
+        }
+        response = requests.post(Url, data=data)
+        with open(f'C:/Users/awsom/Documents/GitHub/Volunteering_project/files/{count+1}_income_tax.pdf', 'wb') as f:
+            f.write(response.content)
         Details.append(det)
+        count += 1
 print(len(Details))
 # save the data in an excel file
 df = pd.DataFrame(Details)
